bbdac784852e2ac3793a74e2698ad00e08e3de19
Grigorii Horos <horosgrisa@gmail.com>
2016-11-23 01:52:42 +0200

    Optimize packages

--- .gulp-tasks/lint/todo.js
+++ .gulp-tasks/lint/todo.js
@@ -3,9 +3,16 @@
 module.exports = function (gulp, plumber, using, gIf, touch) {
   gulp.task('lint:todo', () => {
     const todo = require('gulp-todo')
-    const print = require('gulp-print')
+    let todos = ''
     return gulp.src([`${global.CONFIG.src}/**/*.{css,js,pug,html,njk}`].concat(global.CONFIG.exclude))
-      .pipe(todo({reporter: 'raw'}))
-      .pipe(print())
+      .pipe(todo({
+        reporter: 'table'
+      }))
+      .on('data', (data) => {
+        todos += data._contents.toString()
+      })
+      .on('end', function () {
+        console.log(todos)
+      })
   })
 }
