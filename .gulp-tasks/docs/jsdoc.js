4edac5353697fa9395bbf27f821834f251e6f63d
Grigorii Horos <horosgrisa@gmail.com>
2016-11-23 03:06:31 +0200

    Optimize packages

--- .gulp-tasks/docs/jsdoc.js
+++ .gulp-tasks/docs/jsdoc.js
@@ -1,10 +1,35 @@
 'use strict'
 
 module.exports = function (gulp, plumber, using, gIf, touch) {
-  gulp.task('docs:jsdoc', () => {
-    const documentation = require('gulp-documentation')
+  gulp.task('docs:jsdoc', (cb) => {
+    const jsdoc = require('gulp-jsdoc3')
+
     return gulp.src([`${global.CONFIG.src}/**/*.{css,js,html,pug}`].concat(global.CONFIG.exclude))
-      .pipe(documentation('html'))
-      .pipe(gulp.dest(`${global.CONFIG.src}/docs/`))
+    .pipe(jsdoc({
+      'tags': {
+        'allowUnknownTags': true,
+        'dictionaries': ['jsdoc']
+      },
+      'source': {
+        'include': [`${global.CONFIG.src}/package.json`],
+        'includePattern': '.js$',
+        'excludePattern': `${global.CONFIG.src}/(node_modules/|docs)`
+      },
+      'plugins': [
+        'plugins/markdown'
+      ],
+      'templates': {
+        'cleverLinks': false,
+        'monospaceLinks': true,
+        'useLongnameInNav': false
+      },
+      'opts': {
+        'destination': `${global.CONFIG.src}/docs/jsdoc`,
+        'encoding': 'utf8',
+        'private': true,
+        'recurse': true,
+        'template': './node_modules/minami'
+      }
+    }, cb))
   })
 }
