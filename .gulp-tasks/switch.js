3a082403c0f590bc3530ae65cb74b03b7b74d684
Grigorii Horos <horosgrisa@gmail.com>
2016-11-26 02:28:08 +0200

    Fixes

--- .gulp-tasks/switch.js
+++ .gulp-tasks/switch.js
@@ -5,6 +5,14 @@ const path = require('path')
 
 module.exports = function (gulp, $, argv) {
   gulp.task('switch:project', (done) => {
+    var project = ''
+    try {
+      fs.accessSync(path.join(__dirname, '..', '.selected'), fs.R_OK)
+      project = fs.readFileSync(path.join(__dirname, '..', '.selected'), 'utf8').replace(/\n$/, '')
+    } catch (err) {
+      project = 'example'
+    }
+
     const sysDirs = ['.bin', '.git', '.generator', '.gulp-tasks', 'node_modules', '.examples']
     const dirs = fs.readdirSync(path.join(__dirname, '..')).filter(function (file) {
       return fs.statSync(path.join(__dirname, '..', file)).isDirectory() && sysDirs.indexOf(file) === -1
@@ -14,7 +22,8 @@ module.exports = function (gulp, $, argv) {
         type: 'list',
         name: 'project',
         message: 'What project do you want to enable?',
-        choices: dirs
+        choices: dirs,
+        default: project
       }
     ]).then(function (answers) {
       fs.writeFileSync(path.join(__dirname, '..', '.selected'), answers.project)
