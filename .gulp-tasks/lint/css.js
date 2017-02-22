91f17c8c55a2d90fb9f2356850b28bd271a5727e
Grigorii Horos <horosgrisa@gmail.com>
2017-02-22 12:52:58 +0200

    Fix

--- .gulp-tasks/lint/css.js
+++ .gulp-tasks/lint/css.js
@@ -2,8 +2,6 @@ module.exports = (gulp, $, argv) => {
   gulp.task('lint:css', () => gulp.src([`${global.CONFIG.src}/**/*.css`])
     .pipe($.postcss([
       require('stylelint')(),
-      require('postcss-bem-linter'),
-      require('doiuse'),
       require('postcss-reporter')({ clearMessages: true })
     ])))
 }
